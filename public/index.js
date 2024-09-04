f7834b9096a8d4cd875d01f89b235f26d6577134
NatsumiGONDA <g2210272@gl.cc.uec.ac.jp>
2024-09-04 09:30:45 +0900

    「爽やか！！」の文字を表示

--- public/index.js
+++ public/index.js
@@ -156,3 +156,37 @@ function gabage_click(event) {
     InsectNetElement.remove();
   }, 500);
 }
+
+function bubbles() {
+  // すべての".particletext.bubbles"要素を取得
+  const elements = document.querySelectorAll(".particletext.bubbles");
+  elements.forEach(function (element) {
+    const bubblecount = (element.offsetWidth / 50) * 10;
+
+    // 0からbubblecountまでのループを実行
+    for (let i = 0; i <= bubblecount; i++) {
+      const size = Math.floor(Math.random() * 10 + 10);
+
+      // 新しいバブル(span)を作成
+      const bubble = document.createElement("span");
+      bubble.classList.add("particle");
+
+      // ランダムな位置とサイズ、アニメーションディレイを設定
+      const top = Math.floor(Math.random() * 30 + 30);
+      const left = Math.floor(Math.random() * 95);
+      const delay = Math.random() * 3;
+
+      // スタイルを適用
+      bubble.style.top = `${top}%`;
+      bubble.style.left = `${left}%`;
+      bubble.style.width = `${size}px`;
+      bubble.style.height = `${size}px`;
+      bubble.style.animationDelay = `${delay}s`;
+
+      // span要素を追加
+      element.appendChild(bubble);
+    }
+  });
+}
+
+bubbles();
