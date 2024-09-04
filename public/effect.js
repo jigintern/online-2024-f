adcd1837750af18c8ac22aa186f11ec9ea8a9a64
NaYo01 <nyo08412@gmail.com>
2024-09-04 13:46:22 +0900

    爽やかという文字が出た時に音が出るようにしました
    public/effect.jsのshowSawayakaEffectから関数を呼び出すようにして実装しました。

---
+++ public/effect.js
@@ -0,0 +1,57 @@
+//爽やかエフェクト
+function bubbles() {
+  // すべての".particletext.bubbles"要素を取得
+  const elements = document.querySelectorAll(".particletext.bubbles");
+  elements.forEach(function (element) {
+    const bubblecount = (element.offsetWidth / 50) * 10;
+
+    // 0からbubblecountまでのループを実行
+    for (let i = 0; i <= bubblecount; i++) {
+      const size = Math.floor(Math.random() * 5 + 5);
+
+      // 新しいバブル(span)を作成
+      const bubble = document.createElement("span");
+      bubble.classList.add("particle");
+
+      // ランダムな位置とサイズ、アニメーションディレイを設定
+      const top = Math.floor(Math.random() * 30 - 15);
+      const left = Math.floor(Math.random() * 95);
+      const delay = Math.random() * 3;
+
+      // スタイルを適用
+      bubble.style.top = `${top}%`;
+      bubble.style.left = `${left}%`;
+      bubble.style.width = `${size}px`;
+      bubble.style.height = `${size}px`;
+      bubble.style.animationDelay = `${delay}s`;
+
+      // span要素を追加
+      element.appendChild(bubble);
+    }
+  });
+}
+
+//「爽やか！！」の表示
+function showSawayakaEffect(x, y) {
+  const sawayakaText = document.querySelector(".textcontainer");
+  sawayakaText.style.display = "block";
+  //ゴミがあった場所に表示
+  sawayakaText.style.left = `${x}px`;
+  sawayakaText.style.top = `${y - 10}px`;
+  bubbles();
+  callSawayakasound(); //効果音呼び出し.
+  //2秒間だけ表示
+  setTimeout(function () {
+    sawayakaText.style.display = "none";
+    bubbles.forEach((bubble) => bubble.remove());
+  }, 2000);
+}
+
+function callSawayakasound() {
+  //効果音の再生.
+  audio = document.getElementById("sawayakasound");
+  audio.volume = 0.1;
+  audio.pause();
+  audio.currentTime = 0; //ここと、この上の行で今の音の再生をなくしてもう一度音を出せるようにしている.
+  audio.play();
+}
