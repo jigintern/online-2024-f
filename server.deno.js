d57dd35148a462c112b0f52999d1b1459cec1dd6
haruyuki_16278 <haruyuki@relicsnow.info>
2024-09-05 11:37:05 +0900

    🚿 利用されていないAPI処理を削除

--- server.deno.js
+++ server.deno.js
@@ -4,10 +4,6 @@ Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
-  }
-
   return serveDir(req, {
     fsRoot: "public",
     urlRoot: "",
